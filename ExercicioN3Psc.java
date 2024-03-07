d7e0fe0f137474c63eafc8ca21e12721091f7075
DIEGO JUNIO <161660889+Djunioor@users.noreply.github.com>
2024-03-07 19:42:32 -0300

    Add files via upload

---
+++ ExercicioN3Psc.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExercicioN3Psc {
+
+
+    public static void main(String[] args) throws Exception {
+        String nome;
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println(}Digite seu nome: ");
+        nome = leitor.next();
+
+        System.out.print("Bem vindo: " + nome);
+    }
+}
+
